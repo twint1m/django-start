38942c981c5cb079fcb8a43576d314eee642fa7f
twint1m <twint1m.contact@gmail.com>
2023-06-15 18:36:17 +0300

    6

--- djangoCourse/main/views.py
+++ djangoCourse/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'main page!'})
 
 
 def about(request):
